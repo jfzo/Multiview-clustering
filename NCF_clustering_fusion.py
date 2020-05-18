56eb02abe04791ddb9f1d01f219af56c2cfaf2de
JZ <juanzox@gmail.com>
2020-05-18 19:53:19 -0400

    cxPoint fixed

--- NCF_clustering_fusion.py
+++ NCF_clustering_fusion.py
@@ -167,11 +167,11 @@ def cxTwoPointCopy(ind1, ind2):
     #print("cut point 1:", cxpoint1)
     #print("cut point 2:", cxpoint2)
 
-    offspr_1 = np.array(ind1)
-    offspr_2 = np.array(ind2)
+    offspr_1 = ind1.copy()
+    offspr_2 = ind2.copy()
     
-    offspr_1[cxpoint1:cxpoint2] = np.array(ind2[cxpoint1:cxpoint2])
-    offspr_2[cxpoint1:cxpoint2] = np.array(ind1[cxpoint1:cxpoint2])
+    offspr_1[cxpoint1:cxpoint2] = ind2[cxpoint1:cxpoint2].copy()
+    offspr_2[cxpoint1:cxpoint2] = ind1[cxpoint1:cxpoint2].copy()
     #offspr_1[cxpoint1:cxpoint2], offspr_2[cxpoint1:cxpoint2] \
     #= ind2[cxpoint1:cxpoint2].copy(), ind1[cxpoint1:cxpoint2].copy()
         
